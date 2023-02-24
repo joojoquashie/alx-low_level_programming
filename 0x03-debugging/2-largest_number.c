c158153b39f06bd3121691c36a13e13418a0b012
joojoquashie <joojoquashie@gmail.com>
2023-02-24 05:07:18 -0800

    Largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,15 +13,15 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a >= b && a >= c)
 	{
 	largest = a;
 	}
-	else if (a > b && c > a)
+	else if (a >= b && c >= a)
 	{
 	largest = c;
 	}
-	else if (b > c)
+	else if (b >= c)
 	{
 	largest = b;
 	}
