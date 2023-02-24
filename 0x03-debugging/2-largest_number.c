5f60699a77b05e3afdac334c4471cdd64010904f
joojoquashie <joojoquashie@gmail.com>
2023-02-23 19:23:57 -0800

    The largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,32 +9,37 @@
  */
 
 int largest_number(int a, int b, int c)
+
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a > b && a > c)
 	{
-		largest = a;
+	largest = a;
 	}
-	else if (a > c && c > b)
+	else if (a > b && c > a)
 	{
-		largest = a;
+	largest = c;
 	}
-	else if (b > a && a > c)
+	else if (b > c)
 	{
-		largest = b;
-	}
-	else if (b > c && c > a)
-	{
-		largest = b;
-	}
-	else if (c > a && a > b)
-	{
-		largest = c;
+	largest = b;
 	}
 	else
 	{
-		largest = c;
+	largest = c;
 	}
+
 	return (largest);
 }
+
+
+
+
+
+
+
+
+
+
+
